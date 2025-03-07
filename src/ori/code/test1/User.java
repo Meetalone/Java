638b3414202ef3c233c52ff71dabc71d58876841
ori <viieta0111@gmail.com>
2025-03-07 20:35:03 +0800

    day01

---
+++ src/ori/code/test1/User.java
@@ -0,0 +1,106 @@
+package ori.code.test1;
+
+public class User {
+    private String username;
+    private String pwd;
+    private String email;
+    private int sex;
+    private int age;
+
+
+    public User() {
+    }
+
+    public User(String username, String pwd, String email, int sex, int age) {
+        this.username = username;
+        this.pwd = pwd;
+        this.email = email;
+        this.sex = sex;
+        this.age = age;
+    }
+
+    /**
+     * 获取
+     * @return username
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * 设置
+     * @param username
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /**
+     * 获取
+     * @return pwd
+     */
+    public String getPwd() {
+        return pwd;
+    }
+
+    /**
+     * 设置
+     * @param pwd
+     */
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+
+    /**
+     * 获取
+     * @return email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * 设置
+     * @param email
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * 获取
+     * @return sex
+     */
+    public int getSex() {
+        return sex;
+    }
+
+    /**
+     * 设置
+     * @param sex
+     */
+    public void setSex(int sex) {
+        this.sex = sex;
+    }
+
+    /**
+     * 获取
+     * @return age
+     */
+    public int getAge() {
+        return age;
+    }
+
+    /**
+     * 设置
+     * @param age
+     */
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String toString() {
+        return "User{username = " + username + ", pwd = " + pwd + ", email = " + email + ", sex = " + sex + ", age = " + age + "}";
+    }
+}
+
---
+++ src/ori/string/test2/Test.java
@@ -0,0 +1,11 @@
+package ori.string.test2;
+
+public class Test {
+    public static void main(String[] args) {
+        StringTest st = new StringTest("abcd");
+        System.out.println(st.length());
+        System.out.println(st.charAt(0));
+
+
+    }
+}
